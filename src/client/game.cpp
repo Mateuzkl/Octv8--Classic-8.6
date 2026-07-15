1abdc2c0016104aebb98d87368d4acd244c84b17
G.Joker👻 <125809602+Mateuzkl@users.noreply.github.com>
2026-07-14 23:42:03 -0300

    feat(weather): support zone weather

--- src/client/game.cpp
+++ src/client/game.cpp
@@ -22,6 +22,7 @@
 
 #include "game.h"
 #include "localplayer.h"
+#include "weathermanager.h"
 #include "map.h"
 #include "tile.h"
 #include "creature.h"
@@ -74,6 +75,7 @@ void Game::terminate()
 
 void Game::resetGameStates()
 {
+    g_weatherManager.clear();
     m_online = false;
     m_denyBotCall = false;
     m_dead = false;
